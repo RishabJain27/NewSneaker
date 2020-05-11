f10e9a65f15d802d619d557d86d29077ed2c1797
Rishab Jain <rjain9@ucsc.edu>
2020-05-10 22:51:06 -0700

    updated version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Shoe from "./Shoe"
 import Nike from "./Nike";
 import Adidas from "./Adidas";
+import Underarmour from "./Underarmour";
+
+
 const App = () => {
   
+  const [shoes, setShoes] = useState([]);
   const [nikes, setNike] = useState([]);
   const [adidas, setAdidas] = useState([]);
+  const [underarmour, setUnderarmour] = useState([]);
 
   useEffect( () => {
-    nikeShoes();
-    adidasShoes();
+    shoesDB();
+    //nikeShoes();
+    //adidasShoes();
+    //underarmourShoes();
   },[]);
+
+  const shoesDB = async () => {
+    const response = await fetch(`http://localhost:3001/shoes`);
+    const data = await response.json();
+    setShoes(data);
+  }
 
   const nikeShoes = async () => {
     const response = await fetch(`http://localhost:3001/nikeShoes`);
@@ -26,9 +40,27 @@ const App = () => {
     //console.log(data);
   }
 
+  const underarmourShoes = async () => {
+    const response = await fetch(`http://localhost:3001/underarmourShoes`);
+    const data = await response.json();
+    setUnderarmour(data);
+    //console.log(data);
+  }
+
   return (
     <div className="App">
       <h1 className="h1">Newly Released Shoes</h1>
+      <div className="shoes">             
+        {shoes.map(shoe => (
+          <Shoe
+            key = {shoe.image_url}
+            names={shoe.name}
+            image_url = {shoe.image_url} 
+            site = {shoe.site}
+            category = {shoe.category}
+          />
+        ))}
+      </div>
       <div className="nikes">             
         {nikes.map(nike => (
           <Nike
@@ -48,6 +80,17 @@ const App = () => {
             image_url = {adidas.image_url} 
             site = {adidas.site}
             category = {adidas.category}
+          />
+        ))}
+      </div>
+      <div className="underarmours">             
+        {underarmour.map(underarmour => (
+          <Underarmour
+            key = {underarmour.image_url}
+            names={underarmour.name}
+            image_url = {underarmour.image_url} 
+            site = {underarmour.site}
+            category = {underarmour.category}
           />
         ))}
       </div>
