e10485a2d3324c575b51cbca8d0584125bf39fc0
samirjain10x <49662923+samirjain10x@users.noreply.github.com>
2020-06-06 15:43:34 -0700

    update

---
+++ build/precache-manifest.0de960453d5980f30569d4bd82586fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379f78ef2dabe4b9ffc8535fd5a6f83c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a60874d2591d72b90ada",
+    "url": "/static/css/main.c5662f8f.chunk.css"
+  },
+  {
+    "revision": "2cc1e1a8309ace6f8671",
+    "url": "/static/js/2.931bb405.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.931bb405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60874d2591d72b90ada",
+    "url": "/static/js/main.9821665e.chunk.js"
+  },
+  {
+    "revision": "ce7bef145cfb1654428a",
+    "url": "/static/js/runtime-main.3643e72a.js"
+  }
+]);
